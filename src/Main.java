a33267de81fcb1f4f98386d1d14213aedca308d3
Nelia Terrell <nterrell1014@gmail.com>
2023-01-30 02:32:43 -0500

    more practice

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package HelloWorld;
 
 import java.awt.*;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -18,37 +19,82 @@ public class Main {
 
         //-------PRIMITIVE VS REFERENCE TYPE
 
-            // primitive
+        // primitive
 //            byte age = 30;
 
-            //reference
+        //reference
 //            Date now = new Date();
 //            System.out.println(now);
 
-            //-----primitive----
+        //-----primitive----
 
 //            byte x = 1;
 //            byte y = x;
 //            x = 2;
 //        System.out.println(y); // 1
 
-            //-----REFERENCE-----
+        //-----REFERENCE-----
 
 //            Point point1 = new Point(x:1, y:1);
 //            Point point2 = point1;
 //            point1.x = 2;
 //            System.out.println(point2);  //Point(x:2, y:1)
 
-            // --------STRINGS-------
+        // --------STRINGS-------
 
-             String message = "Hello World" + "!!";
-//           //System.out.println(message);
+//             String message = "Hello World" + "!!";
+////           //System.out.println(message);
+//
+//            System.out.println(message.endsWith("!!"));  // true
+//            System.out.println(message.startsWith("!!"));  //false
+//            System.out.println(message.length());    //13
+//            System.out.println(message.indexOf("l"));  // 2 (first l it can find)
+//            System.out.println(message.replace("!", "*"));  //Hello World**
+//            System.out.println(message.toLowerCase());  //hello world!!
+//            System.out.println(message.toUpperCase());   //HELLO WORLD!!
+//            System.out.println(message.trim());   ///It removes white spaces before and after a string
 
-            System.out.println(message.endsWith("!!"));  // true
-            System.out.println(message.startsWith("!!"));  //false
-            System.out.println(message.length());    //13
-            System.out.println(message.indexOf("l"));  // 2 (first l it can find)
-            System.out.println(message.replace("!", "*"));  //Hello World**
+        //-----------SPEACIAL CHARACTERS--------
+
+        // -------\" --------
+
+//              String message = "Hello \"Nelia\"";
+//              System.out.println(message);   //Hello "Nelia"
+
+        // ------ \\--------
+
+//              String message = "c:\\Windows\\...";
+//              System.out.println(message);   //c:\Windows\...
+
+        // ---------\n-------- (new line)
+//                String message = "Hello \n Nelia";
+//                System.out.println(message);  // Hello (first line), Nelia (next line)
+
+        // ---------\t--------(adds space or tab)
+
+//                String message = "Hi\t Nelia";
+//                System.out.println(message);  // Hi    Nelia
+
+        //--------ARRAYS------
+
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//
+//        System.out.println(Arrays.toString(numbers));  // [1 , 2, 0, 0, 0] === last 3 are zero by default
+//                                                        // index 0 and 1 were initialized as 1 and 2
+
+
+        int[] numbers = {2, 3, 5, 1, 4};
+
+        System.out.println(numbers.length);   // 5
+
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers)); // 1, 2, 3, 4, 5
+
+
+
     }
 }
 
